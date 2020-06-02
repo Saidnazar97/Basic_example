845791d8e2adb5c0cf3d0bd3308d2badae105cff
Saidnazar97 <lifedada98@gmail.com>
2020-06-01 19:48:33 -0700

    test

--- test/util.test.js
+++ test/util.test.js
@@ -1,9 +1,25 @@
-const { add } = require("../util/util");
+const { add, minus } = require("../util/util");
 
 describe("Add", () => {
   describe("Success", () => {
     it("Should return the sum of the 2 passed arguments", () => {
-      expect(add(2, 2).toBe(3));
+      expect(add(2, 2)).toBe(4);
+    });
+  });
+
+  describe("Fail", () => {
+    it("Should throw an error if the first argument is not a number", () => {
+      expect(() => add("2", 2)).toThrowError();
+    });
+
+    it("Should throw an error if the second argument is not a number", () => {
+      expect(() => add(2, "2")).toThrowError();
+    });
+
+    it("Should throw an error if no arguments were recieved", () => {
+      expect(() => add()).toThrowError();
     });
   });
 });
+
+describe("Minus", () => {});
